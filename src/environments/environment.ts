b6de4d4af514db8f45190af0a0df3997e500398e
maescristro <milfortchristian@gmail.com>
2018-03-01 17:08:23 +0100

    modif commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDx69ujzgtL2ijbIUWjtHEH0r0YygNX5D0",
+    authDomain: "angularcrud-3809c.firebaseapp.com",
+    databaseURL: "https://angularcrud-3809c.firebaseio.com",
+    projectId: "angularcrud-3809c",
+    storageBucket: "",
+    messagingSenderId: "316863969704"
+  }
 };
